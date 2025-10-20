3a05368bd8346c6a7ced939d3ddee890c03c907b
toctoce <toctoce@naver.com>
2025-10-20 21:09:44 +0900

    feat(ApplicationTest): 테스트코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -13,14 +13,84 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
+            run(InputData.INPUT_WITH_DELIMITER1);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITH_DELIMITER2);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITH_DELIMITER3);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITH_DELIMITER4);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITH_DELIMITER5);
+            assertThat(output()).contains("결과 : 1");
         });
+    }
+
+    @Test
+    void 기본_입력() {
+        assertSimpleTest(() -> {
+            run(InputData.INPUT_WITHOUT_DELIMITER1);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITHOUT_DELIMITER2);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITHOUT_DELIMITER3);
+            assertThat(output()).contains("결과 : 10");
+            run(InputData.INPUT_WITHOUT_DELIMITER4);
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITH_DELIMITER1))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITH_DELIMITER2))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITH_DELIMITER3))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITH_DELIMITER4))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_입력_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITHOUT_DELIMITER1))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITHOUT_DELIMITER2))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITHOUT_DELIMITER3))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(InputData.WRONG_INPUT_WITHOUT_DELIMITER4))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
