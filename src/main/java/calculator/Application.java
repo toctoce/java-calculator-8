ce6d724f64ead33b08b425050fd2f95ec6cc9ad9
toctoce <toctoce@naver.com>
2025-10-20 17:56:32 +0900

    feat(Application): 문자열 덧셈 계산기 기능 구현
    InputReader, Calculator, OutputWriter, InputParser의 기능을 연결시켜 문자열 덧셈 계산기 기능을 구현함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        Calculator calculator = new Calculator();
+        OutputWriter outputWriter = new OutputWriter();
+        InputParser inputParser = new InputParser();
+
+        String input = inputReader.read();
+        List<Long> numberList = inputParser.parse(input);
+        Long addResult = calculator.add(numberList);
+        outputWriter.writeResult(addResult);
     }
 }
